6d7e1cd702ef07d1d8e4a5b0a896a3af66e7970d
ma6174 <ma617495@gmail.com>
2015-01-11 18:27:14 +0800

    add ReadAt

--- slow_reader.go
+++ slow_reader.go
@@ -33,15 +33,41 @@ func (s *SlowReadSeeker) Seek(offset int64, whence int) (int64, error) {
 	return s.Reader.Seek(offset, whence)
 }
 
-func NewReadSeeker(ReadSeeker io.ReadSeeker, duration time.Duration) *SlowReadSeeker {
-	return &SlowReadSeeker{
-		SlowReader: NewReader(ReadSeeker, duration),
-		Reader:     ReadSeeker,
+type SlwoReaderAt struct {
+	Reader   io.ReaderAt
+	Duration time.Duration
+}
+
+func (s *SlwoReaderAt) ReadAt(p []byte, off int64) (n int, err error) {
+	t := make([]byte, 1)
+	for i := 0; i < len(p); i++ {
+		tn, err := s.Reader.ReadAt(t, off)
+		if err != nil {
+			return n, err
+		}
+		p[i] = t[0]
+		n += tn
+		time.Sleep(s.Duration)
+	}
+	return
+}
+
+func NewReader(reader io.Reader, duration time.Duration) *SlowReader {
+	return &SlowReader{
+		Reader:   reader,
+		Duration: duration,
 	}
 }
-func NewReader(Reader io.Reader, duration time.Duration) *SlowReader {
-	return &SlowReader{
-		Reader:   Reader,
+func NewReadSeeker(readSeeker io.ReadSeeker, duration time.Duration) *SlowReadSeeker {
+	return &SlowReadSeeker{
+		SlowReader: NewReader(readSeeker, duration),
+		Reader:     readSeeker,
+	}
+}
+
+func NewReaderAt(readerAt io.ReaderAt, duration time.Duration) *SlwoReaderAt {
+	return &SlwoReaderAt{
+		Reader:   readerAt,
 		Duration: duration,
 	}
 }
